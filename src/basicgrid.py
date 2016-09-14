b352ac709ac0516f3564d654f7541973d570fb7d
Ilya Vladimirsky <ivv.user@gmail.com>
2016-09-14 23:22:47 +0300

    [~{upd}] #3 docs for BasicGrid class

--- src/basicgrid.py
+++ src/basicgrid.py
@@ -2,8 +2,21 @@ from exceptions.grid_exceptions import OutOfCellsBoundError
 
 
 class BasicGrid:
+    """
+    Represents the grid.
+    """
 
     def __init__(self, height, width, margin, cells_in_row):
+        """
+        Initializes grid properties.
+
+        Args:
+            height (:obj:`int`): Height of the cell in pixels.
+            width (:obj:`int`): Width of the cell in pixels.
+            margin (:obj:`int`): Distance between cells in pixels.
+            cells_in_row (:obj:`int`): Count of the cells in row.
+        """
+
         self.cell_height = height
         self.cell_width = width
         self.margin = margin
@@ -16,9 +29,9 @@ class BasicGrid:
         """
         Draws the grid on the screen using pygame object.
 
-        :param screen: object of the screen
-        :param pygame: object of the pygame module
-        :return: None
+        Args:
+            :obj:`screen`: specified the screen where a snake will be drawn.
+            :obj:`pygame`: used to call ``draw`` method.
         """
 
         for row in range(self.cells_in_row):
@@ -36,7 +49,8 @@ class BasicGrid:
         between cells, width and height of each cell and count of cells in row/column
         as well.
 
-        :return: the list of calculated screen coordinates
+        Returns:
+             :obj:`list`: calculated screen coordinates.
         """
 
         width = self.margin + self.cells_in_row * (self.cell_width + self.margin)
@@ -45,11 +59,7 @@ class BasicGrid:
         return [width, height]
 
     def clear(self):
-        """
-        Set all cells in the grid to default color.
-
-        :return: None
-        """
+        """Set all cells in the grid to default color."""
 
         for row in self._grid:
             for cell in row:
@@ -59,7 +69,12 @@ class BasicGrid:
         """
         Converts screen coordinates to the coordinates of the cell in the grid.
 
-        :return tuple of cell coordinates
+        Raises:
+            OutOfCellsBoundError: if screen coordinates are negative or between
+            cells.
+
+        Returns:
+             :obj:`tuple`: converted cell coordinates.
         """
 
         x_remainder = x % (self.cell_width + self.margin)
@@ -79,31 +94,29 @@ class BasicGrid:
         """
         Set the color of the cell with coordinates x and y.
 
-        :param color: tuple meaning the color in RGB format
-        :param x: x coordinate of setting cell
-        :param y: y coordinate of setting cell
-        :return: None
+        Params:
+            :obj:`tuple` color: the color in RGB format
+            :obj:`int` x: x coordinate of setting cell
+            :obj:`int` y: y coordinate of setting cell
         """
 
         self._grid[x][y] = color
 
     def _grid_init(self):
-        """
-        Creates 2-dimensional array contained the color of each cell in the grid
-        and init all cells by default color (BLACK).
-
-        :return: 2-dimensional array of default colours
-        """
-
         grid = [[self.default_color for x in range(self.cells_in_row)] for y in range(self.cells_in_row)]
 
         return grid
 
     def is_cell_coordinates_out_of_grid(self, cell_x, cell_y):
         """
-        :param cell_x: x coordinate of the cell
-        :param cell_y: y coordinate of the cell
-        :return: True if coordinates are out of grid else False
+         Verifies if the coordinates are out of grid.
+
+        Args:
+            :obj:`int` cell_x: x coordinate of the cell
+            :obj:`int` cell_y: y coordinate of the cell
+
+        Returns:
+             True if coordinates are out of grid else False.
         """
 
         if cell_x < 0 or cell_y < 0 or cell_x >= self.cells_in_row or cell_y >= self.cells_in_column:
@@ -113,26 +126,17 @@ class BasicGrid:
 
     @property
     def grid(self):
+        """:obj:`list` of :obj:`list`: grid"""
         return self._grid.copy()
 
     @property
     def default_color(self):
-        """
-        Return default color of a grid
-
-        :return: color as tuple
-        """
+        """:obj:`tuple`: color in RGB format"""
 
         return self._default_color
 
     @default_color.setter
     def default_color(self, red, green, blue):
-        """
-        Set default color of a grid
-
-        :param new_color: color as tuple
-        :return: None
-        """
 
         self.default_color = (red, green, blue)
 
